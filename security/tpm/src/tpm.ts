16fe303ad64d7f8e912f2aaf1ad11916492a56bd
Anthony Ercolano <anthonyvercolano@outlook.com>
2018-01-04 09:25:56 -0800

    Adjust function types and limit scopes.

--- security/tpm/src/tpm.ts
+++ security/tpm/src/tpm.ts
@@ -13,53 +13,25 @@ import * as dbg from 'debug';
 
 const debug = dbg('azure-iot-security-tpm:TpmSecurityClient');
 
-
-const aes128SymDef = new tss.TPMT_SYM_DEF_OBJECT(TPM_ALG_ID.AES, 128, TPM_ALG_ID.CFB);
-
-const ekPersHandle: TPM_HANDLE = new TPM_HANDLE(0x81010001);
-const srkPersHandle: TPM_HANDLE = new TPM_HANDLE(0x81000001);
-const idKeyPersHandle: TPM_HANDLE = new TPM_HANDLE(0x81000100);
-
-// Template of the Endorsement Key
-const ekTemplate = new TPMT_PUBLIC(TPM_ALG_ID.SHA256,
-  TPMA_OBJECT.restricted | TPMA_OBJECT.decrypt | TPMA_OBJECT.fixedTPM | TPMA_OBJECT.fixedParent | TPMA_OBJECT.adminWithPolicy | TPMA_OBJECT.sensitiveDataOrigin,
-  new Buffer('837197674484b3f81a90cc8d46a5d724fd52d76e06520b64f2a1da1b331469aa', 'hex'),
-  new tss.TPMS_RSA_PARMS(aes128SymDef, new tss.TPMS_NULL_ASYM_SCHEME(), 2048, 0),
-  new tss.TPM2B_PUBLIC_KEY_RSA());
-
-// Template of the Storage Root Key
-const srkTemplate = new TPMT_PUBLIC(TPM_ALG_ID.SHA256,
-  TPMA_OBJECT.restricted | TPMA_OBJECT.decrypt | TPMA_OBJECT.fixedTPM | TPMA_OBJECT.fixedParent | TPMA_OBJECT.noDA | TPMA_OBJECT.userWithAuth | TPMA_OBJECT.sensitiveDataOrigin,
-  null,
-  new tss.TPMS_RSA_PARMS(aes128SymDef, new tss.TPMS_NULL_ASYM_SCHEME(), 2048, 0),
-  new tss.TPM2B_PUBLIC_KEY_RSA());
-
-
-interface PersistentKeyInformation {
-  /**
-   * Human readable name for debugging/logging purposes only
-   */
-  name: string;
-
-  /**
-   * The TPM hierarachy, to which the key belongs
-   * @note All the keys are persisted in the owner hierarchy disregarding their mother one
-   */
-  hierarchy: TPM_HANDLE;
-
-  /**
-   * Handle value where the persistent key is expected to be found
-   */
-  handle: TPM_HANDLE;
-
-  /**
-   * Template to be used for key cretaion if the persistent key with the given handle does not exist
-   */
-  template: TPMT_PUBLIC;
-}
-
-
 export class TpmSecurityClient  {
+
+  private static readonly _aes128SymDef: tss.TPMT_SYM_DEF_OBJECT = new tss.TPMT_SYM_DEF_OBJECT(TPM_ALG_ID.AES, 128, TPM_ALG_ID.CFB);
+
+  private static readonly _ekPersistentHandle: TPM_HANDLE = new TPM_HANDLE(0x81010001);
+  private static readonly _srkPersistentHandle: TPM_HANDLE = new TPM_HANDLE(0x81000001);
+  private static readonly _idKeyPersistentHandle: TPM_HANDLE = new TPM_HANDLE(0x81000100);
+
+  private static readonly _ekTemplate: TPMT_PUBLIC = new TPMT_PUBLIC(TPM_ALG_ID.SHA256,
+    TPMA_OBJECT.restricted | TPMA_OBJECT.decrypt | TPMA_OBJECT.fixedTPM | TPMA_OBJECT.fixedParent | TPMA_OBJECT.adminWithPolicy | TPMA_OBJECT.sensitiveDataOrigin,
+    new Buffer('837197674484b3f81a90cc8d46a5d724fd52d76e06520b64f2a1da1b331469aa', 'hex'),
+    new tss.TPMS_RSA_PARMS(TpmSecurityClient._aes128SymDef, new tss.TPMS_NULL_ASYM_SCHEME(), 2048, 0),
+    new tss.TPM2B_PUBLIC_KEY_RSA());
+
+  private static readonly _srkTemplate: TPMT_PUBLIC = new TPMT_PUBLIC(TPM_ALG_ID.SHA256,
+    TPMA_OBJECT.restricted | TPMA_OBJECT.decrypt | TPMA_OBJECT.fixedTPM | TPMA_OBJECT.fixedParent | TPMA_OBJECT.noDA | TPMA_OBJECT.userWithAuth | TPMA_OBJECT.sensitiveDataOrigin,
+    null,
+    new tss.TPMS_RSA_PARMS(TpmSecurityClient._aes128SymDef, new tss.TPMS_NULL_ASYM_SCHEME(), 2048, 0),
+    new tss.TPM2B_PUBLIC_KEY_RSA());
 
   private _ek: TPMT_PUBLIC = null;
   private _srk: TPMT_PUBLIC = null;
@@ -70,8 +42,10 @@ export class TpmSecurityClient  {
 
 
   constructor(registrationId?: string, customTpm?: any) {
+    /*Codes_SRS_NODE_TPM_SECURITY_CLIENT_06_002: [The `customTpm` argument, if present` will be used at the underlying TPM provider.  Otherwise the TPM provider will the tss TPM client with a parameter of `false` for simulator use.] */
     this._tpm = customTpm ? customTpm : new Tpm(false);
     if (registrationId) {
+      /*Codes_SRS_NODE_TPM_SECURITY_CLIENT_06_001: [The `registrationId` argument if present will be returned as the `registrationId` for subsequent calls to `getRegistrationId`.] */
       this._registrationId = registrationId;
     }
     this._fsm = new machina.Fsm({
@@ -135,19 +109,18 @@ export class TpmSecurityClient  {
         connecting: {
           _onEnter: (callback) => {
             try {
-              let self = this;
-              this._tpm.connect(function (): void {
-                self._createPersistentPrimary({ name: 'EK', hierarchy: tss.Endorsement, handle: ekPersHandle, template: ekTemplate }, (ekCreateErr, ekPublicKey) => {
+              this._tpm.connect(() => {
+                this._createPersistentPrimary('EK', tss.Endorsement, TpmSecurityClient._ekPersistentHandle, TpmSecurityClient._ekTemplate, (ekCreateErr: Error, ekPublicKey: TPMT_PUBLIC) => {
                   if (ekCreateErr) {
-                    self._fsm.transition('disconnected', callback, ekCreateErr);
+                    this._fsm.transition('disconnected', callback, ekCreateErr);
                   } else {
-                    self._ek = ekPublicKey;
-                    self._createPersistentPrimary({ name: 'SRK', hierarchy: tss.Owner, handle: srkPersHandle, template: srkTemplate }, (srkCreateErr: Error, srkPublicKey: TPMT_PUBLIC) => {
+                    this._ek = ekPublicKey;
+                    this._createPersistentPrimary('SRK', tss.Owner, TpmSecurityClient._srkPersistentHandle, TpmSecurityClient._srkTemplate, (srkCreateErr: Error, srkPublicKey: TPMT_PUBLIC) => {
                       if (srkCreateErr) {
-                        self._fsm.transition('disconnected', callback, srkCreateErr);
+                        this._fsm.transition('disconnected', callback, srkCreateErr);
                       } else {
-                        self._srk = srkPublicKey;
-                        self._fsm.transition('connected', callback);
+                        this._srk = srkPublicKey;
+                        this._fsm.transition('connected', callback);
                       }
                     });
                   }
@@ -194,11 +167,11 @@ export class TpmSecurityClient  {
     });
   }
 
-  getEndorsementKey(callback: (err: Error, endorsementKey: string) => void): void {
+  getEndorsementKey(callback: (err: Error, endorsementKey: Buffer) => void): void {
       this._fsm.handle('getEndorsementKey', callback);
   }
 
-  getStorageRootKey(callback: (err: Error, storageKey: string) => void): void {
+  getStorageRootKey(callback: (err: Error, storageKey: Buffer) => void): void {
     this._fsm.handle('getStorageRootKey', callback);
   }
 
@@ -221,32 +194,39 @@ export class TpmSecurityClient  {
 
   getRegistrationId(callback: (err: Error, registrationId: string) => void): void {
     if (this._registrationId) {
+      /*Codes_SRS_NODE_TPM_SECURITY_CLIENT_06_003: [If the TpmSecurityClient was given a `registrationId` at creation, that `registrationId` will be returned.] */
       callback(null, this._registrationId);
     } else {
-      this.getEndorsementKey( function (endorsementError: Error, endorsementKey: string): void {
+      this.getEndorsementKey( (endorsementError: Error, endorsementKey: Buffer) => {
         if (endorsementError) {
+          /*Codes_SRS_NODE_TPM_SECURITY_CLIENT_06_005: [Any errors from interacting with the TPM hardware will cause an SecurityDeviceError to be returned in the err parameter of the callback.] */
           callback(endorsementError, null);
         } else {
-          let hasher = crypto.createHash('sha256');
+        /*Codes_SRS_NODE_TPM_SECURITY_CLIENT_06_004: [If not provided, the `registrationId` will be constructed and returned as follows:
+          The endorsementKey will be queried.
+          The endorsementKey will be hashed utilizing SHA256.
+          The resultant digest will be bin32 encoded in conformance with the `RFC4648` specification.
+          The resultant string will have terminating `=` characters removed.] */
+          const hasher = crypto.createHash('sha256');
           hasher.update(endorsementKey);
           this._registrationId = (base32Encode(hasher.digest(), 'RFC4648').toLowerCase()).replace(/=/g, '');
           callback(null, this._registrationId);
         }
-      }.bind(this));
+      });
     }
   }
 
-  private _createPersistentPrimary(pki: PersistentKeyInformation, callback: (err: Error, resultPublicKey: TPMT_PUBLIC) => void): void {
-    this._tpm.allowErrors().ReadPublic(pki.handle, (resp: tss.ReadPublicResponse) => {
+  private _createPersistentPrimary(name: string, hierarchy: TPM_HANDLE, handle: TPM_HANDLE, template: TPMT_PUBLIC, callback: (err: Error, resultPublicKey: TPMT_PUBLIC) => void): void {
+    this._tpm.allowErrors().ReadPublic(handle, (resp: tss.ReadPublicResponse) => {
       let rc = this._tpm.getLastResponseCode();
-      debug('ReadPublic(' + pki.name + ') returned ' + TPM_RC[rc] +  (rc === TPM_RC.SUCCESS ? '; PUB: ' + resp.outPublic.toString() : ''));
+      debug('ReadPublic(' + name + ') returned ' + TPM_RC[rc] +  (rc === TPM_RC.SUCCESS ? '; PUB: ' + resp.outPublic.toString() : ''));
       if (rc !== TPM_RC.SUCCESS) {
-        this._tpm.withSession(tss.NullPwSession).CreatePrimary(pki.hierarchy, new tss.TPMS_SENSITIVE_CREATE(), pki.template, null, null, (resp: tss.CreatePrimaryResponse) => {
-          debug('CreatePrimary(' + pki.name + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()] + '; pub size: ' + (resp.outPublic.unique as tss.TPM2B_PUBLIC_KEY_RSA).buffer.length);
-          this._tpm.withSession(tss.NullPwSession).EvictControl(tss.Owner, resp.handle, pki.handle, () => {
-            debug('EvictControl(0x' + resp.handle.handle.toString(16) + ', 0x' + pki.handle.handle.toString(16) + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
+        this._tpm.withSession(tss.NullPwSession).CreatePrimary(hierarchy, new tss.TPMS_SENSITIVE_CREATE(), template, null, null, (resp: tss.CreatePrimaryResponse) => {
+          debug('CreatePrimary(' + name + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()] + '; pub size: ' + (resp.outPublic.unique as tss.TPM2B_PUBLIC_KEY_RSA).buffer.length);
+          this._tpm.withSession(tss.NullPwSession).EvictControl(tss.Owner, resp.handle, handle, () => {
+            debug('EvictControl(0x' + resp.handle.handle.toString(16) + ', 0x' + handle.handle.toString(16) + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
             this._tpm.FlushContext(resp.handle, () => {
-              debug('FlushContext(TRANSIENT_' + pki.name + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
+              debug('FlushContext(TRANSIENT_' + name + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
               callback(null, resp.outPublic);
             });
           });
@@ -259,16 +239,16 @@ export class TpmSecurityClient  {
 
   private _signData(dataToSign: Buffer, callback: (err: Error, signedData: Buffer) => void): void {
 
-    let idKeyHashAlg: TPM_ALG_ID = (<tss.TPMS_SCHEME_HMAC>(<tss.TPMS_KEYEDHASH_PARMS>this._idKeyPub.parameters).scheme).hashAlg;
+    const idKeyHashAlg: TPM_ALG_ID = (<tss.TPMS_SCHEME_HMAC>(<tss.TPMS_KEYEDHASH_PARMS>this._idKeyPub.parameters).scheme).hashAlg;
 
     this._tpm.GetCapability(tss.TPM_CAP.TPM_PROPERTIES, TPM_PT.INPUT_BUFFER, 1, (caps: tss.GetCapabilityResponse) => {
-      let props = <tss.TPML_TAGGED_TPM_PROPERTY>caps.capabilityData;
+      const props = <tss.TPML_TAGGED_TPM_PROPERTY>caps.capabilityData;
       if (props.tpmProperty.length !== 1 || props.tpmProperty[0].property !== TPM_PT.INPUT_BUFFER) {
-        callback(new errors.DeviceRegistrationFailedError('Unexpected result of TPM2_GetCapability(TPM_PT.INPUT_BUFFER)'), null);
+        callback(new errors.SecurityDeviceError('Unexpected result of TPM2_GetCapability(TPM_PT.INPUT_BUFFER)'), null);
       } else {
-        let maxInputBuffer: number = props.tpmProperty[0].value;
+        const maxInputBuffer: number = props.tpmProperty[0].value;
         if (dataToSign.length <= maxInputBuffer) {
-          this._tpm.withSession(tss.NullPwSession).HMAC(idKeyPersHandle, dataToSign, idKeyHashAlg, (signature: Buffer) => {
+          this._tpm.withSession(tss.NullPwSession).HMAC(TpmSecurityClient._idKeyPersistentHandle, dataToSign, idKeyHashAlg, (signature: Buffer) => {
             callback(null, signature);
           });
         } else {
@@ -288,7 +268,7 @@ export class TpmSecurityClient  {
               });
             }
           };
-          this._tpm.withSession(tss.NullPwSession).HMAC_Start(idKeyPersHandle, signature, idKeyHashAlg, (hSeq: TPM_HANDLE) => {
+          this._tpm.withSession(tss.NullPwSession).HMAC_Start(TpmSecurityClient._idKeyPersistentHandle, signature, idKeyHashAlg, (hSeq: TPM_HANDLE) => {
             console.log('HMAC_Start() returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
             hSequence = hSeq;
             loopFn();
@@ -328,7 +308,7 @@ export class TpmSecurityClient  {
       this._tpm.StartAuthSession(null, null, nonce, null, tss.TPM_SE.POLICY, tss.NullSymDef, TPM_ALG_ID.SHA256, (resp: tss.StartAuthSessionResponse) => {
         debug('StartAuthSession(POLICY_SESS) returned ' + TPM_RC[this._tpm.getLastResponseCode()] + '; sess handle: ' + resp.handle.handle.toString(16));
         if (this._tpm.getLastResponseCode() !== TPM_RC.SUCCESS) {
-          activateCallback(new errors.DeviceRegistrationFailedError('Authorization session unable to be created.'));
+          activateCallback(new errors.SecurityDeviceError('Authorization session unable to be created.  RC value: ' + TPM_RC[this._tpm.getLastResponseCode()].toString()));
         } else {
           let policySession = new tss.Session(resp.handle, resp.nonceTPM);
 
@@ -339,7 +319,7 @@ export class TpmSecurityClient  {
           this._tpm.withSession(tss.NullPwSession).PolicySecret(tss.Endorsement, policySession.SessIn.sessionHandle, null, null, null, 0, (resp: tss.PolicySecretResponse) => {
             debug('PolicySecret() returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
             if (this._tpm.getLastResponseCode() !== TPM_RC.SUCCESS) {
-              activateCallback(new errors.DeviceRegistrationFailedError('Upable to apply the necessary policy to authorize the EK.'));
+              activateCallback(new errors.SecurityDeviceError('Upable to apply the necessary policy to authorize the EK.  RC value: ' + TPM_RC[this._tpm.getLastResponseCode()].toString()));
             } else {
 
               //
@@ -347,10 +327,10 @@ export class TpmSecurityClient  {
               // of the duplication blob of the new Device ID key generated by DRS.
               //
 
-              this._tpm.withSessions(tss.NullPwSession, policySession).ActivateCredential(srkPersHandle, ekPersHandle, credentialBlob, encodedSecret.secret, (innerWrapKey: Buffer) => {
+              this._tpm.withSessions(tss.NullPwSession, policySession).ActivateCredential(TpmSecurityClient._srkPersistentHandle, TpmSecurityClient._ekPersistentHandle, credentialBlob, encodedSecret.secret, (innerWrapKey: Buffer) => {
                 debug('ActivateCredential() returned ' + TPM_RC[this._tpm.getLastResponseCode()] + '; innerWrapKey size ' + innerWrapKey.length);
                 if (this._tpm.getLastResponseCode() !== TPM_RC.SUCCESS) {
-                  activateCallback(new errors.DeviceRegistrationFailedError('Upable to decrypt the symmetric key used to protect duplication blob.'));
+                  activateCallback(new errors.SecurityDeviceError('Upable to decrypt the symmetric key used to protect duplication blob.  RC value: ' + TPM_RC[this._tpm.getLastResponseCode()].toString()));
                 } else {
 
                   //
@@ -363,36 +343,36 @@ export class TpmSecurityClient  {
                   // Import the new Device ID key issued by DRS to the device's TPM
                   //
 
-                  this._tpm.withSession(tss.NullPwSession).Import(srkPersHandle, innerWrapKey, this._idKeyPub, idKeyDupBlob, encWrapKey.secret, symDef, (idKeyPriv: TPM2B_PRIVATE) => {
+                  this._tpm.withSession(tss.NullPwSession).Import(TpmSecurityClient._srkPersistentHandle, innerWrapKey, this._idKeyPub, idKeyDupBlob, encWrapKey.secret, symDef, (idKeyPriv: TPM2B_PRIVATE) => {
                     debug('Import() returned ' + TPM_RC[this._tpm.getLastResponseCode()] + '; idKeyPriv size ' + idKeyPriv.buffer.length);
                     if (this._tpm.getLastResponseCode() !== TPM_RC.SUCCESS) {
-                      activateCallback(new errors.DeviceRegistrationFailedError('Upable to import the device id key into the TPM.'));
+                      activateCallback(new errors.SecurityDeviceError('Upable to import the device id key into the TPM.  RC value: ' + TPM_RC[this._tpm.getLastResponseCode()].toString()));
                     } else {
 
                       //
                       // Load the imported key into the TPM
                       //
 
-                      this._tpm.withSession(tss.NullPwSession).Load(srkPersHandle, idKeyPriv, this._idKeyPub, (hIdKey: TPM_HANDLE) => {
+                      this._tpm.withSession(tss.NullPwSession).Load(TpmSecurityClient._srkPersistentHandle, idKeyPriv, this._idKeyPub, (hIdKey: TPM_HANDLE) => {
                         debug('Load() returned ' + TPM_RC[this._tpm.getLastResponseCode()] + '; ID key handle: 0x' + hIdKey.handle.toString(16));
                         if (this._tpm.getLastResponseCode() !== TPM_RC.SUCCESS) {
-                          activateCallback(new errors.DeviceRegistrationFailedError('Upable to load the device id key into the TPM.'));
+                          activateCallback(new errors.SecurityDeviceError('Upable to load the device id key into the TPM.  RC value: ' + TPM_RC[this._tpm.getLastResponseCode()].toString()));
                         } else {
 
                           //
                           // Remove possibly existing persistent instance of the previous Device ID key
                           //
 
-                          this._tpm.allowErrors().withSession(tss.NullPwSession).EvictControl(tss.Owner, idKeyPersHandle, idKeyPersHandle, () => {
+                          this._tpm.allowErrors().withSession(tss.NullPwSession).EvictControl(tss.Owner, TpmSecurityClient._idKeyPersistentHandle, TpmSecurityClient._idKeyPersistentHandle, () => {
 
                             //
                             // Persist the new Device ID key
                             //
 
-                            this._tpm.withSession(tss.NullPwSession).EvictControl(tss.Owner, hIdKey, idKeyPersHandle, () => {
-                              console.log('EvictControl(0x' + hIdKey.handle.toString(16) + ', 0x' + idKeyPersHandle.handle.toString(16) + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
+                            this._tpm.withSession(tss.NullPwSession).EvictControl(tss.Owner, hIdKey, TpmSecurityClient._idKeyPersistentHandle, () => {
+                              console.log('EvictControl(0x' + hIdKey.handle.toString(16) + ', 0x' + TpmSecurityClient._idKeyPersistentHandle.handle.toString(16) + ') returned ' + TPM_RC[this._tpm.getLastResponseCode()]);
                               if (this._tpm.getLastResponseCode() !== TPM_RC.SUCCESS) {
-                                activateCallback(new errors.DeviceRegistrationFailedError('Upable to persist the device id key into the TPM.'));
+                                activateCallback(new errors.SecurityDeviceError('Upable to persist the device id key into the TPM.  RC value: ' + TPM_RC[this._tpm.getLastResponseCode()].toString()));
                               } else {
 
                                 //
--- security/tpm/test/_tpm_test.js
+++ security/tpm/test/_tpm_test.js
@@ -2,45 +2,109 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 'use strict';
-
+var common = require("azure-iot-common");
 var TpmSecurityClient  = require('../lib/tpm').TpmSecurityClient ;
+var tss = require("tss.js");
+var tss_js_1 = require("tss.js");
+var sinon = require('sinon');
+
+
 var assert = require('chai').assert;
 
+var fakeSimpleTpmClient = {
+  allowErrors: () => {return this},
+  connect: (callback) => {callback();},
+  ReadPublic: (handle, callback) => {
+    callback(null,null);
+  },
+  fakeSimpleTpmClient: () => {},
+  getLastResponseCode: () => {
+    return tss_js_1.TPM_RC.SUCCESS;
+  }
+
+}
 describe('tpm', function () {
   this.timeout(1000);
 
-  var obj = new TpmSecurityClient ();
+  // var obj = new TpmSecurityClient ('MYREGID');
 
   describe('getEndorsementKey', function() {
-    it ('throws', function() {
-      assert.throws(function() {
-        obj.getEndorsementKey();
+    it('returns the endorsement key', function(done) {
+      var client = new TpmSecurityClient(undefined, fakeSimpleTpmClient );
+      var persistStub = sinon.stub(client,'_createPersistentPrimary');
+      persistStub.withArgs('EK').callsArgWith(4, null, TpmSecurityClient._ekTemplate);
+      persistStub.withArgs('SRK').callsArgWith(4, null, TpmSecurityClient._srkTemplate);
+      client.getEndorsementKey((err, localEk) => {
+        assert.deepEqual(localEk, TpmSecurityClient._ekTemplate.asTpm2B(), 'Invalid endorsment key returned.');
+        done();
       });
     });
   });
 
   describe('getStorageRootKey', function() {
-    it ('throws', function() {
-      assert.throws(function() {
-        obj.getStorageRootKey();
+    it('returns the storage root key', function(done) {
+      var client = new TpmSecurityClient(undefined, fakeSimpleTpmClient );
+      var persistStub = sinon.stub(client,'_createPersistentPrimary');
+      persistStub.withArgs('EK').callsArgWith(4, null, TpmSecurityClient._ekTemplate);
+      persistStub.withArgs('SRK').callsArgWith(4, null, TpmSecurityClient._srkTemplate);
+      client.getStorageRootKey((err, localSrk) => {
+        assert.deepEqual(localSrk, TpmSecurityClient._srkTemplate.asTpm2B(), 'Invalid storage root key returned.');
+        done();
       });
     });
   });
 
-  describe('signWithIdentity', function() {
-    it ('throws', function() {
-      assert.throws(function() {
-        obj.signWithIdentity();
-      });
-    });
-  });
+  // describe('signWithIdentity', function() {
+  //   it ('throws', function() {
+  //     assert.throws(function() {
+  //       obj.signWithIdentity();
+  //     });
+  //   });
+  // });
 
-  describe('activateSymmetricIdentity', function() {
-    it ('throws', function() {
-      assert.throws(function() {
-        obj.activateSymmetricIdentity();
+  // describe('activateSymmetricIdentity', function() {
+  //   it ('throws', function() {
+  //     assert.throws(function() {
+  //       obj.activateSymmetricIdentity();
+  //     });
+  //   });
+  // });
+
+  describe('getRegistrationId', function() {
+    /*Tests_SRS_NODE_TPM_SECURITY_CLIENT_06_003: [If the TpmSecurityClient was given a `registrationId` at creation, that `registrationId` will be returned.] */
+    it('returns original id', function(done) {
+      var providedRegistrationClient = new TpmSecurityClient('MYREGID', fakeSimpleTpmClient );
+      providedRegistrationClient.getRegistrationId((err, id) => {
+        assert.strictEqual(id, 'MYREGID', 'Incorrect registration Id.' );
+        done();
       });
     });
+
+    it('returns constructed registration id', function(done) {
+      /*Tests_SRS_NODE_TPM_SECURITY_CLIENT_06_004: [If not provided, the `registrationId` will be constructed and returned as follows:
+        The endorsementKey will be queried.
+        The endorsementKey will be hashed utilizing SHA256.
+        The resultant digest will be bin32 encoded in conformance with the `RFC4648` specification.
+        The resultant string will have terminating `=` characters removed.] */
+      var providedRegistrationClient = new TpmSecurityClient(undefined, fakeSimpleTpmClient );
+      sinon.stub(providedRegistrationClient,'getEndorsementKey').callsArgWith(0, null, 'MYREGID');
+      providedRegistrationClient.getRegistrationId((err, id) => {
+        assert.strictEqual(id, 'vfn2bxtbqwc3pcflozty5reiunt5qm4ztk4ulrszujmqj3zbei2a', 'Incorrect registration Id.' );
+        done();
+      });
+    });
+
+    it('handles an error', function(done) {
+      /*Tests_SRS_NODE_TPM_SECURITY_CLIENT_06_005: [Any errors from interacting with the TPM hardware will cause an InvalidOperationError to be returned in the err parameter of the callback.] */
+      var errorFromGetEndorsement = new common.errors.InvalidOperationError('Error from hardware');
+      var providedRegistrationClient = new TpmSecurityClient(undefined, fakeSimpleTpmClient );
+      sinon.stub(providedRegistrationClient,'getEndorsementKey').callsArgWith(0, errorFromGetEndorsement, null);
+      providedRegistrationClient.getRegistrationId((err, id) => {
+        assert.strictEqual(err, errorFromGetEndorsement, 'Improper error returned');
+        done();
+      });
+    });
+
   });
 
 });
